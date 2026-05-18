0015fc6d73d5729fe4e85ee4907f0627f6061803
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-05-19 00:25:14 +0200

    ex3

---
+++ ex3/ft_vault_security.py
@@ -0,0 +1,35 @@
+def secure_archive(filename: str,
+                   mode: int = 1, text: str = "") -> tuple[bool, str]:
+    if mode == 1:
+        try:
+            with open(filename) as file:
+                data = file.read()
+                return (True, data)
+        except Exception as e:
+            return (False, str(e))
+    else:
+        try:
+            with open(filename, "w+") as file:
+                file.write(text)
+                return (True, "Content successfully written to file")
+        except Exception as e:
+            return (False, str(e))
+
+
+def main() -> None:
+    print("Using 'secure_archive' to read from a nonexistent file:")
+    print(secure_archive("1.txt"))
+    print()
+    print("Using 'secure_archive' to read from an inaccessible file:")
+    print(secure_archive("/etc/master.passwd"))
+    print()
+    print("Using 'secure_archive' to read from a regular file:")
+    result = secure_archive("test.txt")
+    print(result)
+    print()
+    print("Using 'secure_archive' to write previous content to a new file:")
+    print(secure_archive("new.txt", 0, result[1]))
+
+
+if __name__ == "__main__":
+    main()
