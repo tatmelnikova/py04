a0050f40f81fe468d2864d39932b7fc08efd580e
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-05-19 00:11:18 +0200

    ex2: close files properly

--- ex2/ft_stream_management.py
+++ ex2/ft_stream_management.py
@@ -19,14 +19,25 @@ def main() -> None:
         print("Usage: ft_archive_creation.py <file>")
         return
     filename = argv[1]
+
+    file = None
     try:
         file = open(filename)
         print("---")
         text = file.read()
         print(text)
         print("---")
-        file.close()
-        print(f"File {filename} closed")
+    except Exception as e:
+        print(f"[STDERR] Error opening file '{filename}':", e, file=sys.stderr)
+    finally:
+        if file is not None:
+            file.close()
+            print(f"File {filename} closed")
+        else:
+            print("no file for reading, return")
+            return
+
+    try:
         print("Transform data:")
         print("---")
         lines = text.split("\n")
@@ -36,17 +47,25 @@ def main() -> None:
             print(new_line)
             new_text = new_text + new_line + "\n"
         print("---")
-        output_name = get_filename()
-        if output_name:
-            print(f"Saving data to '{output_name}'")
-            output_file = open(output_name, "w+")
+    except Exception as e:
+        print("[STDERR] Error transforming the text", e, file=sys.stderr)
+        return
+
+    output_file = None
+    try:
+        out_name = get_filename()
+        if out_name:
+            print(f"Saving data to '{out_name}'")
+            output_file = open(out_name, "w+")
             output_file.write(new_text)
-            output_file.close()
-            print(f"Data saved in file '{output_name}'.")
+            print(f"Data saved in file '{out_name}'.")
         else:
             print("Not saving data.")
     except Exception as e:
-        print(f"[STDERR] Error opening file '{filename}':", e, file=sys.stderr)
+        print(f"[STDERR] Error opening file '{out_name}':", e, file=sys.stderr)
+    finally:
+        if output_file is not None:
+            output_file.close()
 
 
 if __name__ == "__main__":
