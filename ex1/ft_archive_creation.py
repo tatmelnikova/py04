b7f646aaca0fa66f637f3d51ad36a3c7684794d3
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-05-19 00:16:23 +0200

    fix ex1

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -10,34 +10,46 @@ def main() -> None:
         print("Usage: ft_archive_creation.py <file>")
         return
     filename = argv[1]
+
+    file = None
     try:
         file = open(filename)
         print("---")
         text = file.read()
         print(text)
         print("---")
-        file.close()
-        print(f"File {filename} closed")
-        print("Transform data:")
-        print("---")
-        lines = text.split("\n")
-        new_text = ""
-        for line in lines:
-            new_line = line + "#"
-            print(new_line)
-            new_text = new_text + new_line + "\n"
-        print("---")
+    except Exception as e:
+        print(f"Error opening file '{filename}':", e)
+    finally:
+        if file is not None:
+            file.close()
+            print(f"File {filename} closed")
+
+    print("Transform data:")
+    print("---")
+    lines = text.split("\n")
+    new_text = ""
+    for line in lines:
+        new_line = line + "#"
+        print(new_line)
+        new_text = new_text + new_line + "\n"
+    print("---")
+
+    out_file = None
+    try:
         output_name = input("Enter new file name (or empty):")
         if output_name:
             print(f"Saving data to '{output_name}'")
-            output_file = open(output_name, "w+")
-            output_file.write(new_text)
-            output_file.close()
+            out_file = open(output_name, "w+")
+            out_file.write(new_text)
             print(f"Data saved in file '{output_name}'.")
         else:
             print("Not saving data.")
     except Exception as e:
         print(f"Error opening file '{filename}':", e)
+    finally:
+        if out_file is not None:
+            out_file.close()
 
 
 if __name__ == "__main__":
